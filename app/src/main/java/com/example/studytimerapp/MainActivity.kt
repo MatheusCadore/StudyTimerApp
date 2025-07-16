09a84cfd8b9232d7f05c5bed8e39288110c6f827
matheus <matheusecadore@gmail.com>
2025-07-15 23:24:22 -0300

    Start

--- app/src/main/java/com/example/studytimerapp/MainActivity.kt
+++ app/src/main/java/com/example/studytimerapp/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.studytimerapp.ui.theme.StudyTimerAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,8 +33,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            Timer()
         }
+    }
+}
+
+@Preview
+@Composable
+fun Timer(){
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.padding(6.dp)) {
+            Text(text = "00:00", textAlign = TextAlign.Center, fontSize = 60.sp, modifier = Modifier.fillMaxWidth())
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth()) {
+                Button(onClick = {} ) {
+                    Text("Start")
+                }
+                Button(onClick = {} ) {
+                    Text("Reset")
+                }
+            }
+        }
+
     }
 }
 
